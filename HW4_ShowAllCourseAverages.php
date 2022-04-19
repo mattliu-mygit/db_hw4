f35ec5aca0e9ed404bc09067bfe2a03d1b63d3e9
Matthew Liu <mliu78@jh.edu>
2022-04-19 18:35:43 -0400

    task 4 works

--- HW4_ShowAllCourseAverages.php
+++ HW4_ShowAllCourseAverages.php
@@ -6,7 +6,7 @@
     include "open.php";
 	  $password = $_POST['password'];
     $sqlPassCheck = "Call HW4_CHECKPASSWORD('". $password."');";
-	  echo "<h2>All Raw Scores for ".$password."</h2><br>";
+	  echo "<h2>Raw Scores for ".$password."</h2><br>";
     try {
       // Password check.
       $passCheck = $conn->query($sqlPassCheck);
@@ -17,55 +17,79 @@
       try {
         // Connection opened and closed because ran into issues with multiple query ommands not executing well.
         $conn->close();
-        include "open.php"; 
+        include "open.php";
         $sqlAssignmentCheck = "Call HW4_GETASSIGNMENTS();";
         $assignments = $conn->query($sqlAssignmentCheck);
         $assignmentArray = mysqli_fetch_all($assignments);
+        $totalExam = 0;
+        $totalQuiz  = 0;
 
-        $sql = "Call HW4_SHOWALLRAWSCORES();";
-        echo "<table border=\"1px solid black\">";
-        $header = "<tr><th> SID </th> <th> lname </th> <th> fname </th> <th> section </th>";
-
-        // Update headers with all assignment names
+        // Update the total possible exam and quiz scores
         foreach($assignments as $assignment) {
-          $header=$header."<th>".$assignment["AName"]."</th>";
+          if ($assignment["AType"] == "EXAM") {
+            $totalExam = $totalExam + $assignment["PtsPoss"];
+          } else {
+            $totalQuiz = $totalQuiz + $assignment["PtsPoss"];
+          }
         }
-        echo $header."</tr>";
 
         $conn->close();
         include "open.php";
 
+        $sql = "Call HW4_SHOWALLRAWSCORES();";
+        echo "<table border=\"1px solid black\">";
+        echo "<tr><th> SID </th> <th> lname </th> <th> fname </th> <th> section </th> <th> courseAvg </th></tr>";
+
+
         $content = "";
         try {
           if ($result = $conn->query($sql)) {
+            // Initial state setup
             $check = true;
             $indexMod = 0; // keeps track of index positioning after encountering null values
+            $examEarned = 0;
+            $quizEarned = 0;
             foreach($result as $index => $row){
-              // At the beginning of each row, start a new table row with student information
+              // Every new row, start a new table row with student information
               if ($check) {
                 $content=$content."<tr><td>".$row["SID"]. "</td><td>".$row["LName"]."</td><td>".$row["FName"]."</td><td>".$row["Sec"];
                 $check = false;
               }
+              // Handle Assessments
               $indexTot = ($index+$indexMod) % $assignments->num_rows ;
-
-              // Skip over untaken assessments while making sure we make sure to check for the end of a row
               while ($row["AName"] != $assignmentArray[$indexTot][0]) {
-                $content=$content."</td><td>";
-                // If we are at the end of a row, start a new row
+                $type = $assignmentArray[$indexTot][1];
+                // Update course average and make a new row
                 if (($indexTot+1)% $assignments->num_rows == 0) {
-                  $content=$content."</td></tr>";
+                  // Set weight-calculated course average
+                  $total = 0.4 * ($quizEarned/$totalQuiz) + 0.6 * ($examEarned/$totalExam);
+                  $content=$content."</td><td>".round($total,2)."</td></tr>";
+                  // Cleanup
+                  $quizEarned = 0;
+                  $examEarned = 0;
                   $check = true;
                 }
+                // Skip one for assessments not taken.
                 $indexMod = $indexMod + 1;
                 $indexTot =  ($index+$indexMod) % $assignments->num_rows;
               }
 
-              // records score for each assignment
+              // Update the total earned points for the current assignment
               $score = $row["Score"];
-              $content=$content."</td><td>".$row["Score"];
-              // If we are at the end of a row, start a new row
+              if ($row["AType"] == "EXAM") {
+                $examEarned = $examEarned + $score;
+              } else {
+                $quizEarned = $quizEarned + $score;
+              }
+
+              // Update course average and make a new row
               if (($indexTot+1)% $assignments->num_rows == 0) {
-                $content=$content."</td></tr>";
+                // Set weight-calculated course average
+                $total = 0.4 * ($quizEarned/$totalQuiz) + 0.6 * ($examEarned/$totalExam);
+                $content=$content."</td><td>".round($total,2)."</td></tr>";
+                // Cleanup
+                $quizEarned = 0;
+                $examEarned = 0;
                 $check = true;
               }
             }
