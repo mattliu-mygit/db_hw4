89496830de6df133de621194102ce5b84f68b5be
Matthew Liu <mliu78@jh.edu>
2022-04-18 21:03:54 -0400

    Update HW4_ShowRawScores.php

--- HW4_ShowRawScores.php
+++ HW4_ShowRawScores.php
@@ -5,21 +5,15 @@
   <?php
     include "open.php";
     // $sql = file_get_contents('HW4_SHOWRAWSCORES.sql');
-    $sql = "Select HW4_Student.SID,
-    Lname,
-    FName,
-    Sec,
-    AName,
-    Score
+    $sql = "Select *
   FROM HW4_Student
-    JOIN HW4_RawScore ON HW4_Student.SID = HW4_RawScore.SID
-  WHERE HW4_Student.SID = 1006;";
+    JOIN HW4_RawScore ON HW4_Student.SID = HW4_RawScore.SID;";
 
     	//execute the query, then run through the result table row by row to
       //put each row's data into our array
       if ($result = mysqli_query($conn,$sql)){
         foreach($result as $row){
-          array_push($dataPoints, array( "SID"=> $row["SID"], "Score"=> $row["Score"]));
+          echo $dataPoints, array( "SID"=> $row["SID"], "Score"=> $row["Score"]);
         }
       }
     
@@ -28,29 +22,3 @@
 
   ?>
 </body>
-
-<html>
-<head>
-<script>
-window.onload = function () { 
-	var chart = new CanvasJS.Chart("chartContainer", {
-		animationEnabled: true,
-		exportEnabled: true,
-		theme: "light1", // "light1", "light2", "dark1", "dark2"
-		title:{
-			text: "PHP Line Chart from Database - MySQLi"
-		},
-		data: [{
-			type: "line", //change type to column, bar, line, area, pie, etc  
-			dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
-		}]
-	});
-	chart.render(); 
-}
-</script>
-</head>
-<body>
-	<div id="chartContainer" style="height: 400px; width: 100%;"></div>
-	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</body>
-</html>
