0131906c32480ac11205b7695a7c30cf80d2941c
Matthew Liu <mliu78@jh.edu>
2022-04-18 21:00:04 -0400

    added chart

--- HW4_ShowRawScores.php
+++ HW4_ShowRawScores.php
@@ -19,3 +19,29 @@
 
   ?>
 </body>
+
+<html>
+<head>
+<script>
+window.onload = function () { 
+	var chart = new CanvasJS.Chart("chartContainer", {
+		animationEnabled: true,
+		exportEnabled: true,
+		theme: "light1", // "light1", "light2", "dark1", "dark2"
+		title:{
+			text: "PHP Line Chart from Database - MySQLi"
+		},
+		data: [{
+			type: "line", //change type to column, bar, line, area, pie, etc  
+			dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+		}]
+	});
+	chart.render(); 
+}
+</script>
+</head>
+<body>
+	<div id="chartContainer" style="height: 400px; width: 100%;"></div>
+	<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
